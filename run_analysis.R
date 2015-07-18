2b20fa8c1c63df6f61d968330f97eec142b26e48
John O'Brien <obrien.jk@gmail.com>
2015-07-18 15:12:32 +0100

    Save as work-in-progress

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 # This script performs the analysis required for the assignment. It is broken
 # into five sections, each corresponding to the five steps listed in the
 # assignment instructions. The order of those steps has been changed slightly
-# and a zeroth step has been added. The order of steps is given here.
+# and a zeroth step has been added. The new order of steps is given here.
 
 # 0) Download and unpack the data zip file.
 # 1) Merge the training and the test sets to create one data set.
@@ -13,7 +13,7 @@
 #    with the average of each variable for each activity and each subject.
 
 
-#####                                                                      #####
+#####                     Get Data Files                                   #####
 #                                                                              #
 # In this section the script looks for the project zip file and unzips it. If  #
 # the zip file is missing it downloads it from the original source and then    #
@@ -32,7 +32,6 @@ url <- paste0(
     "getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 )
 
-
 # Make sure the zip file is present.
 if (!file.exists(zip_file)) {
 
@@ -43,7 +42,10 @@ if (!file.exists(zip_file)) {
 
 # Then unzip the contents.
 message("Unzipping...")
-unzip(zip_file)
+#unzip(zip_file)
+
+# Clean up objects that will no longer be needed.
+rm(list=c("url", "zip_file"))
 
 
 # The data files are all .txt files, so get a list of those.
@@ -56,12 +58,8 @@ data_files <- data_files[grep("CodeBook|README|_info", data_files, invert=TRUE)]
 data_files <- data_files[grep("Inertial", data_files, invert=TRUE)]
 
 
-# Clean up objects that will no longer be needed.
-rm(list=c("url", "zip_file"))
 
-
-
-#####                                                                      #####
+#####                       Merge Data                                     #####
 #                                                                              #
 # In this section the script reads all the text files into R objects. Since    #
 # the basenames of the text files are unique, the script uses these to name    #
@@ -86,39 +84,91 @@ for (i in 1:length(var_names)) {
 }
 
 # Tidy as we go.
-rm(list=c("var_names", "data_files", "i"))
+#rm(list=c("var_names", "data_files", "i"))
 
 
-# Now we have all our data, it's time to start merging it together.
-# The two main objects are X_test and X_train.
-
-# Create variables to identify the data set as test or train.
-label_test  <- data.frame(dset = rep_len("test",  dim(X_test)[1] ) )
-label_train <- data.frame(dset = rep_len("train", dim(X_train)[1]) )
-
-# Add columns to X_test and X_train to identify the subject, the activity, and
-# the data set.
-X_test  <- cbind(subject_test,  y_test,  label_test,  X_test)
-X_train <- cbind(subject_train, y_train, label_train, X_train)
-
-# Then join the "test" and "trial" data sets into one.
-full_set <- rbind(X_test, X_train)
-
-rm(list=c("X_train", "y_train","subject_train", "label_train",
-          "X_test",  "y_test", "subject_test",  "label_test")
+# Now we have all our data, it's time to start merging it together. The two
+# main objects are X_test and X_train. We need to include columns for a subject
+# id, an activity id, and a label for the data set (test/train).
+library(data.table)
+big_dt <- data.table(
+    subj_id     = c(subject_test$V1, subject_train$V1),
+    activity_id = c(y_test$V1,       y_train$V1),
+    set_label   = c(
+        rep_len("test",  nrow(X_test) ),
+        rep_len("train", nrow(X_train))
+    ),
+    rbind(X_test, X_train)
 )
 
-# Some of the feature names have parentheses and hyphens.
-features <- gsub("[()]", "",  features)
-features <- gsub("-",    "_", features)
-
-# Add column names for the subject id, the feature id and the data set labels.
-features <- c("subj_id", "activity_id", "data_set", features)
-
-# Use the cleaned features vector to name the full_set columns
-names(full_set) <- features
+#rm(list=c("X_train", "y_train","subject_train",
+#          "X_test",  "y_test", "subject_test" )
+#)
 
 
 
+#####                     Set Variable Names                               #####
+#                                                                              #
+# The file "UCI HAR Dataset/features.txt" holds the variable names for X_test  #
+# and X_train, but they need a little cleaning up.                             #
+#                                                                              #
+#####                                                                      #####
 
-rm(list=c("activity_lables", "features"))
+
+# Remove parentheses from the feature names.
+features <- gsub("[()]", "",  features$V2)
+
+# Replace commas and hypens with underscores.
+features <- gsub("[,-]", "_", features)
+
+# We already have names for the first three columns.
+end <- length(names(big_dt))
+setnames(big_dt, names(big_dt)[4:end], features)
+
+# Tidy up before moving on.
+#rm("features", "end")
+
+
+#################
+# GOOD TO HERE. #
+# I THINK!      #
+#################
+
+
+
+#####                    Descriptive Activity Labels                       #####
+#                                                                              #
+# The file "UCI HAR Dataset/activity_labels.txt" maps the codes used in the    #
+# data files to descriptive text labels. So use this to replace the codes in   #
+# our "big_dt" data table.                                                     #
+#                                                                              #
+#####                                                                      #####
+
+# Map the activity labels to the id codes in a new vector.
+#act_label <- activity_labels$V2[full_set$activity_id]
+
+# Add the new column on the left of the "full_set" data frame.
+#full_set <- cbind(data.frame(activity=act_label), full_set)
+
+# Keep things tidy.
+#rm(list=c("act_label", "activity_labels"))
+
+
+
+#####                   Extract means & std. deviations                    #####
+#                                                                              #
+# We're only interested in the variables concerning means and standard         #
+# deviations (as well as the subject_id, activity label and data set label, of #
+# course). So make a vector listing just that subset of "full_set" columns and #
+# use it to extract those columns into a smaller data frame.                   #
+#                                                                              #
+#####                                                                      #####
+
+# Use a regexp to make the vector list.
+#regexp <- "activity|_id|_set|mean|_std_"
+#subset <- grep(regexp, names(full_set), ignore.case=TRUE, value=TRUE)
+
+# Pull those columns from "full_set".
+#slim_df <- full_set[,subset]
+
+#rm(list=c("full_set", "subset"))
